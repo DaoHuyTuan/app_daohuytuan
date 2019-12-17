b0c5a6f6f6c791ce4ea2f3e7ae75b62c3e7e8932
daipham1210 <daipham1210@gmail.com>
2019-12-17 16:35:53 +0700

    Update deploy_bash.sh

--- deploy_bash.sh
+++ deploy_bash.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-eval `ssh-agent -s`
-sudo ssh-add /root/.ssh/daipham
 export app='nodeapp_daohuytuan' # container & image name
 path_app="$HOME/daohuytuan"
 gitProjectName="app_daohuytuan"
