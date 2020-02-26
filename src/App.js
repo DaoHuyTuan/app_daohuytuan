91f2ca0fb73b4fdc84d3ea63a7bbe5ad5c1d6a86
Đào Huy Tuấn <tuandh95@gmail.com>
2020-02-26 17:57:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello badman
+         Hello kexau
         </p>
         <a
           className="App-link"
